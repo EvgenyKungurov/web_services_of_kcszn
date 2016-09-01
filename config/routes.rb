ae9a39c85a8342a5f941f1f08212cc6b190e544d
Evgeny Kungurov <evgenii.kungurov@gmail.com>
2016-09-01 13:32:45 +0900

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :eployment_ceters
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: 'dashboard#index'
+  root 'dashboard#index'
 end
